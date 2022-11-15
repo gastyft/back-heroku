a67a8c9eb29b166a28d77ae2424782ec0205e1f4
gastyftacla <99477542+gastyft@users.noreply.github.com>
2022-11-14 22:13:38 -0300

    Update createRoles.java

--- src/main/java/com/miportfolio/security/util/createRoles.java
+++ src/main/java/com/miportfolio/security/util/createRoles.java
@@ -1,4 +1,4 @@
-/*
+
 package com.miportfolio.security.util;
 
 import com.miportfolio.security.entity.Rol;
@@ -22,6 +22,6 @@ public class createRoles implements CommandLineRunner {
          
     }
 } 
-*/
+
 
 

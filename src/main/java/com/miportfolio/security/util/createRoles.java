99fafbaa9d662053db5745594cbdeedd9758ff0c
gastyftacla <99477542+gastyft@users.noreply.github.com>
2023-01-13 05:48:23 -0300

    Update createRoles.java

--- src/main/java/com/miportfolio/security/util/createRoles.java
+++ src/main/java/com/miportfolio/security/util/createRoles.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-/*
+
 @Component
 public class createRoles implements CommandLineRunner {
        @Autowired
@@ -22,7 +22,7 @@ public class createRoles implements CommandLineRunner {
          
     }
 } 
-*/
+
 
 
 

f21d9e27688564a7631235c97c5a0ab638c5bc49
gastyftacla <99477542+gastyft@users.noreply.github.com>
2023-01-15 08:09:09 -0300

    Update PersonaController.java

--- src/main/java/com/miportfolio/controller/PersonaController.java
+++ src/main/java/com/miportfolio/controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="https://portfolio-yo-programo.web.app")
+ @CrossOrigin(origins="https://portfolio-yo-programo.web.app")
 
 public class PersonaController {
     
